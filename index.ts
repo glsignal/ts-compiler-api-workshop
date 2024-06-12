471631d8f1cd1ea180d27bcde32ad4e6217f622e
Greg Signal <greg@cardboardstraw.com>
2024-06-12 14:51:50 +0300

    checkpoint

--- index.ts
+++ index.ts
@@ -19,7 +19,34 @@ function printSource(sourceFile: ts.SourceFile) {
   console.log(result);
 }
 
+const transformFunction = (node: ts.ArrowFunction): ts.FunctionExpression => {
+  return ts.factory.createFunctionExpression(
+    node.modifiers,
+    node.asteriskToken,
+    node.name,
+    node.typeParameters,
+    node.parameters,
+    node.type,
+    ts.isExpression(node.body)
+      ? ts.factory.createBlock([ts.factory.createReturnStatement(node.body)])
+      : node.body
+  );
+};
+
 const transformDeclaration: ts.Transformer<ts.VariableDeclaration> = (node) => {
+  if (
+    ts.isFunctionLike(node.initializer) &&
+    ts.isArrowFunction(node.initializer)
+  ) {
+    return ts.factory.updateVariableDeclaration(
+      node,
+      node.name,
+      node.exclamationToken,
+      node.type,
+      transformFunction(node.initializer)
+    );
+  }
+
   if (ts.isIdentifier(node.name) && node.name.text === "hello") {
     return ts.factory.updateVariableDeclaration(
       node,
@@ -65,7 +92,20 @@ const transformer: ts.TransformerFactory<ts.SourceFile> =
     return ts.visitEachChild(sourceFile, transformNode, context);
   };
 
-// const world = 'world';
+// export const hello = (text?: string) => {
+//   console.log(`Hello, ${text ?? "World!"}`);
+// };
+//
+// let world = <T>(t: T): T => {
+//     return t;
+//   },
+//   another = () => {
+//     console.log("another");
+//   },
+//   aThird = () => "test",
+//   a = 5,
+//   b = "string",
+//   c = true;
 for (const root of program.getRootFileNames()) {
   const sourceFile = program.getSourceFile(root);
   if (sourceFile && !sourceFile.isDeclarationFile) {
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-const hello = "world";
+export const hello = (text?: string) => {
+  console.log(`Hello, ${text ?? "World!"}`);
+};
+
+let world = <T>(t: T): T => {
+    return t;
+  },
+  another = () => {
+    console.log("another");
+  },
+  aThird = () => "test",
+  a = 5,
+  b = "string",
+  c = true;
