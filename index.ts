80a00c93508584321a74bdafa2bd8271ff6ea0b8
Greg Signal <greg@cardboardstraw.com>
2024-06-12 14:13:43 +0300

    checkpoint (unfinished exercise to write printer)

--- index.ts
+++ index.ts
@@ -103,4 +103,123 @@ const hello = ts.factory.createFunctionDeclaration(
   )
 );
 
-printCode(variable, hello);
+// import React from "react";
+// import {
+//   PageTitle,
+//   PageSubtitle,
+//   PageBody,
+//   PageTags,
+//   PageTag,
+// } from "my-ui-lib";
+//
+// export default function IntroPage() {
+//   const context = useContext();
+//
+//   return (
+//     <>
+//       <PageTitle>My Page Title</PageTitle>
+//       <PageSubtitle>My Page Subtitle</PageSubtitle>
+//       <PageBody>Hello, world!</PageBody>
+//       <PageTags>
+//         <PageTag>Tag 1</PageTag>
+//         <PageTag>Tag 2</PageTag>
+//       </PageTags>
+//     </>
+//   );
+// }
+interface InputConfig {
+  name: string;
+  title: string;
+  subtitle?: string;
+  path: string;
+  parent: string;
+  content: string;
+  tags?: Array<{ id: string; name: string }>;
+}
+
+function generatePage(input: InputConfig): ts.Node[] {
+  const requireTags = Boolean(input.tags?.length);
+  const requireSubtitle = input.subtitle != null;
+
+  function importSpecifier(name: string) {
+    return ts.factory.createImportSpecifier(
+      false,
+      undefined,
+      ts.factory.createIdentifier(name)
+    );
+  }
+
+  return [
+    ts.factory.createImportDeclaration(
+      undefined,
+      ts.factory.createImportClause(
+        false,
+        ts.factory.createIdentifier("React"),
+        undefined
+      ),
+      ts.factory.createIdentifier("react"),
+      undefined
+    ),
+    ts.factory.createImportDeclaration(
+      undefined,
+      ts.factory.createImportClause(
+        false,
+        undefined,
+        ts.factory.createNamedImports([
+          importSpecifier("PageTitle"),
+          ...(requireSubtitle ? [importSpecifier("PageSubtitle")] : []),
+          importSpecifier("PageBody"),
+          ...(requireTags
+            ? [importSpecifier("Tags"), importSpecifier("Tag")]
+            : []),
+        ])
+      ),
+      ts.factory.createIdentifier("my-ui-lib"),
+      undefined
+    ),
+    ts.factory.createFunctionExpression(
+      [
+        ts.factory.createModifier(ts.SyntaxKind.ExportKeyword),
+        ts.factory.createModifier(ts.SyntaxKind.DefaultKeyword),
+      ],
+      undefined,
+      input.name,
+      undefined,
+      undefined,
+      undefined,
+      ts.factory.createBlock(
+        [
+          ts.factory.createVariableStatement(undefined, [
+            ts.factory.createVariableDeclaration(
+              "context",
+              undefined,
+              undefined,
+              ts.factory.createCallExpression(
+                ts.factory.createIdentifier("useContext"),
+                undefined,
+                undefined
+              )
+            ),
+          ]),
+        ],
+        true
+      )
+    ),
+  ];
+}
+
+const input = {
+  name: "IntroPage",
+  title: "My Page Title",
+  subtitle: "My Page Subtitle",
+  path: "/pages/intro",
+  parent: "cloud",
+  content: "Hello, world!",
+  tags: [
+    { id: "tag1", name: "Tag 1" },
+    { id: "tag2", name: "Tag 2" },
+  ],
+};
+const page = generatePage(input);
+
+printCode(...page);
